f0ae20511750aba68bac4f944a312faf61e98df2
hoangphuc_0062 <bmtck0000@gmail.com>
2024-05-08 20:44:08 +0700

    error handler

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,9 @@ var userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    next();
+  }
   const salt = bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
