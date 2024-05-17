dcf2a978917c54460957f6b6b14e6cbd918a6ae8
hoangphuc_0062 <bmtck0000@gmail.com>
2024-05-17 16:55:02 +0700

    crud api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const userRouter = require("./user");
+const productRouter = require("./product");
+const categoryRouter = require("./catogory");
 const { notFound, errHandler } = require("../middlewares/errHandler");
 const initRoutes = (app) => {
   app.use("/api/users", userRouter);
+  app.use("/api/products", productRouter);
+  app.use("/api/categories", categoryRouter);
 
   app.use(notFound);
   app.use(errHandler);
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const ctrl = require("../controllers/user");
 
-const { verifyAccessToken } = require("../middlewares/vertifyToken");
+const { verifyAccessToken, isAdmin } = require("../middlewares/vertifyToken");
 
 router.post("/register", ctrl.register);
 router.post("/login", ctrl.login);
@@ -11,6 +11,11 @@ router.post("/refreshtoken", ctrl.refreshAccessToken);
 router.get("/logout", ctrl.logout);
 router.get("/forgotpassword", ctrl.forgotPassword);
 router.put("/resetpassword", ctrl.resetPassword);
+// Trong ngoặc vuông để phân biệt middleware
+router.get("/", [verifyAccessToken, isAdmin], ctrl.getUsers);
+router.delete("/", [verifyAccessToken, isAdmin], ctrl.deleteUser);
+router.put("/current", [verifyAccessToken], ctrl.updateUser);
+router.put("/:userId", [verifyAccessToken, isAdmin], ctrl.updateUserByAdmin);
 
 module.exports = router;
 
